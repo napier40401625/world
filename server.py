5d32fda48e0a94af6d69422b2bacc6c8176199e7
napier40401625 <kellvin.ros@gmail.com>
2018-04-06 07:10:41 +0630

    finish

--- server.py
+++ server.py
@@ -14,6 +14,23 @@ def mainPage():
 		page_number = 0,
 		page_size = page_size)
 
+@app.route('/addCountryByName')
+def addCountryByNamePage():
+	n = request.args.get('country')
+	c = {}
+	c['name'] = n
+	c['capital'] = request.args.get('capital')
+	c['continent'] = request.args.get('continent')
+	c['area'] = float(request.args.get('area'))
+	c['gdp'] = float(request.args.get('gdp'))
+	c['tld'] = request.args.get('tld')
+	c['population'] = float(request.args.get('population'))
+	w.append(c)
+	w.sort(key = lambda c: c['name'])
+	return render_template(
+		'country.html',
+		c = c)
+
 @app.route('/begin/<b>')
 def beginPage(b):
 	bn = int(b)
@@ -49,18 +66,23 @@ def countryByNamePage(n):
 		'country.html',
 		c = c)
 
+@app.route('/createCountry')
+def createCountryByName():
+	return render_template('createCountry.html',
+		c = c)
+
 @app.route('/delete/<n>')
 def deleteCountry(n):
-        i = 0
-        for c in w:
-                if c['name']==n:
-                        break
-                i = i + 1
-        del w[i]
-        return render_template('index.html',
-        	page_number=0,
-        	page_size=page_size,
-        	w = w[0:page_size])
+    i = 0
+    for c in w:
+        if c['name']==n:
+            break
+        i = i + 1
+    del w[i]
+    return render_template('index.html',
+        page_number=0,
+        page_size=page_size,
+        w = w[0:page_size])
 
 @app.route('/editCountryByName/<n>')
 def editCountryByNamePage(n):
@@ -73,15 +95,18 @@ def editCountryByNamePage(n):
 
 @app.route('/updateCountryByName')
 def updateCountryByNamePage():
-	n = request.args.get('name')
-	c = None
-	for x in w:
-                if x['name'] == n:
-                        c = x
-	c['capital'] = request.args.get('capital')
-	c['continent'] = request.args.get('continent')
-	return render_template('country.html', c = c)
+    n=request.args.get('name')
+    c = None
 
-app.run(host='0.0.0.0', port=2525, debug=True)
+    for x in w:
+        if x['name'] == n:
+            c = x
+    c['capital'] = request.args.get('capital')
+    c['continent']=request.args.get('continent')
+    return render_template(
+        'country.html',
+        c=c)
+
+app.run(host='0.0.0.0', port=5625, debug=True)
 
 
